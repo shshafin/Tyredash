a1a07edcf7b4e856da06792ed1b62a6dd2fc47a2
webnur <mdnuruli291@gmail.com>
2025-04-25 17:34:40 +0600

    update auth validation in makes

--- backend/src/app/modules/makes/make.route.ts
+++ backend/src/app/modules/makes/make.route.ts
@@ -4,11 +4,14 @@ import validateRequest from "../../middlewares/validateRequest";
 import { MakeController } from "./make.controller";
 import { MakeValidation } from "./make.validation";
 import { uploadImage } from "../../../helpers/fileHandlers";
+import auth from "../../middlewares/auth";
+import { ENUM_USER_ROLE } from "../../../enum/user";
 
 const router = express.Router();
 
 router.post(
   "/create",
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   uploadImage,
   validateRequest(MakeValidation.createMakeZodSchema),
   MakeController.createMake
@@ -20,11 +23,16 @@ router.get("/", MakeController.getAllMakes);
 
 router.patch(
   "/:id",
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   uploadImage,
   validateRequest(MakeValidation.updateMakeZodSchema),
   MakeController.updateMake
 );
 
-router.delete("/:id", MakeController.deleteMake);
+router.delete(
+  "/:id",
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  MakeController.deleteMake
+);
 
 export const MakeRoutes = router;
